85742843053788e39e8660ce9abc8e39850e13c9
hlays <lays.hidani@gmail.com>
2019-02-26 20:11:48 -0300

    add template as funções

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 window.onload = function() {
-  showCards();
+  showAllCards();
 };
 
-const result = document.querySelector("#result");
+const resultAll = document.querySelector("#show-all-cards");
+const searchResul = document.querySelector("#search-result");
 
 const srcBar = document.querySelector("#src-bar");
 const srcBtn = document.querySelector("#src-btn");
@@ -11,40 +12,60 @@ function getPokemons() {
   return POKEMON["pokemon"];
 }
 
-function showCards() {
-  result.innerHTML = `${getPokemons().map((pokemon) => `
+
+function showAllCards() {
+  resultAll.innerHTML = `${getPokemons().map((pokemon) => `
     <div class="pokemon-card">
-      <img src="${pokemon['img']}"></img>
+      <img src="${pokemon["img"]}"></img>
     </div>
     <div class="pokemon-name">
     <h3>${pokemon["name"]}</h3>
     </div>
     <div class="pokemon-info">
       <ul>
-        <li>Tipo: ${pokemon['type']}</li>
-        <li>Altura: ${pokemon['height']}</li>
-        <li>Peso: ${pokemon['weight']}</li>
-        <li>Fraquezas: ${pokemon['weaknesses']}</li>
-        <li>Evoluções: ${pokemon['next_exolution']}</li>
-        <li>Egg: ${pokemon['egg']}</li>
-        <li>Spaw chance: ${pokemon['spawn_chance']}</li>
-        <li>AVG Spawns: ${pokemon['avg_spawns']}</li>
-        <li>Spawns time: ${pokemon['spawn_time']}</li>
-        <li>Multipliers: ${pokemon['multipliers']}</li>
+        <li>Tipo: ${pokemon["type"]}</li>
+        <li>Altura: ${pokemon["height"]}</li>
+        <li>Peso: ${pokemon["weight"]}</li>
+        <li>Egg: ${pokemon["egg"]}</li>
+        <li>Spaw chance: ${pokemon["spawn_chance"]}</li>
+        <li>AVG Spawns: ${pokemon["avg_spawns"]}</li>
+        <li>Spawns time: ${pokemon["spawn_time"]}</li>
+        <li>Fraquezas: ${pokemon["weaknesses"]}</li>
       </ul>
-    </div>
-  `  
-  ).join('')}`
+    </div>`).join("")
+  }`
 }
+
+
 
 function findType(requiredType) {
   for (pokemon of POKEMON["pokemon"]) {
     for (pokeType of pokemon["type"]) {
       if (requiredType.toUpperCase() === pokeType.toUpperCase()) {
-        let newLi = document.createElement("li");
-        newLi.textContent = pokemon["name"] + " -> type: " + pokemon["type"];
+        let newArticle = document.createElement("article");
         
-        resultOne.appendChild(newLi);  
+        newArticle.innerHTML = 
+        `
+        <div class="pokemon-card">
+          <img src="${pokemon["img"]}"></img>
+        </div>
+        <div class="pokemon-name">
+        <h3>${pokemon["name"]}</h3>
+        </div>
+        <div class="pokemon-info">
+          <ul>
+            <li>Tipo: ${pokemon["type"]}</li>
+            <li>Altura: ${pokemon["height"]}</li>
+            <li>Peso: ${pokemon["weight"]}</li>
+            <li>Egg: ${pokemon["egg"]}</li>
+            <li>Spaw chance: ${pokemon["spawn_chance"]}</li>
+            <li>AVG Spawns: ${pokemon["avg_spawns"]}</li>
+            <li>Spawns time: ${pokemon["spawn_time"]}</li>
+            <li>Fraquezas: ${pokemon["weaknesses"]}</li>
+          </ul>
+        </div>
+      `
+        searchResul.appendChild(newArticle);  
       }
     }
   }
@@ -57,17 +78,37 @@ function findWeakness(requiredWeakness) {
   for (pokemon of POKEMON["pokemon"]) {
     for (pokeWeakness of pokemon["weaknesses"]) {
       if (requiredWeakness.toUpperCase() === pokeWeakness.toUpperCase()) {
-        let newLi = document.createElement("li");
-        newLi.textContent = pokemon["name"] + " -> weakenesses: " + pokemon["weaknesses"];
+        let newArticle = document.createElement("article");
         
-        result.appendChild(newLi);  
+        newArticle.innerHTML = 
+        `
+        <div class="pokemon-card">
+          <img src="${pokemon["img"]}"></img>
+        </div>
+        <div class="pokemon-name">
+        <h3>${pokemon["name"]}</h3>
+        </div>
+        <div class="pokemon-info">
+          <ul>
+            <li>Tipo: ${pokemon["type"]}</li>
+            <li>Altura: ${pokemon["height"]}</li>
+            <li>Peso: ${pokemon["weight"]}</li>
+            <li>Egg: ${pokemon["egg"]}</li>
+            <li>Spaw chance: ${pokemon["spawn_chance"]}</li>
+            <li>AVG Spawns: ${pokemon["avg_spawns"]}</li>
+            <li>Spawns time: ${pokemon["spawn_time"]}</li>
+            <li>Fraquezas: ${pokemon["weaknesses"]}</li>
+          </ul>
+        </div>
+      `
+        searchResul.appendChild(newArticle); 
+
       }
     }
   }
 }
 
 // findWeakness("Fire");
-
 
 function findName() {
   let requiredName = srcBar.value;
@@ -78,13 +119,13 @@ function findName() {
       let newLi = document.createElement("li");
       newLi.textContent = pokemon["name"];
       
-      result.appendChild(newLi);  
+      resultAll.appendChild(newLi);  
     }
   }
   // let newLi = document.createElement("li");
   // newLi.textContent = ":/ Pokemon não encontrado...";
   
-  // resultThree.appendChild(newLi);  
+  // resultAllThree.appendChild(newLi);  
 }
 
 srcBtn.addEventListener("click", findName);
