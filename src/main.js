e2b43a15526cf5454213d57a7f15dd814a008caf
hlays <lays.hidani@gmail.com>
2019-03-02 22:13:17 -0300

    atualização do readme

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ const selectedLetter = document.querySelector("#selected-letter");
 const typeMenu = document.querySelector("#type-menu");
 const weaknessesMenu = document.querySelector("#weaknesses-menu");
 const weaknessesBtn = document.querySelector("#weaknesses-btn");
+
 const srcBar = document.querySelector("#src-bar");
 const srcBtn = document.querySelector("#src-btn");
 
@@ -167,46 +168,45 @@ function findWeakness() {
   resultHeader.appendChild(newH);
 }
 
-// function findName() {
-//   let requiredName = srcBar.value;
+function findName() {
+  let requiredName = srcBar.value;
 
-//   let filteredName = pokemons.filter((pokemon) => pokemon["name"].toUpperCase() === requiredName.toUpperCase());
+  pokemons.forEach(function(pokemon) {
+    let name = pokemon.name;
 
-//   console.log(filteredName);
-//   // console.log(filteredName["type"]);
-  
-//   if (filteredName !== undefined) {
-//     let newArticle = document.createElement("article");
+    if (name.toUpperCase() === requiredName.toUpperCase()) {
+
+      let newArticle = document.createElement("article");
         
-//     newArticle.innerHTML = `
-//     <div class="pokemon-card">
-//       <img src="${filteredName["img"]}"></img>
-//     </div>
-//     <div class="pokemon-name">
-//     <h3>${filteredName["name"]}</h3>
-//     </div>
-//     <div class="pokemon-info">
-//       <ul>
-//       <li>Tipo: ${filteredName["type"]}</li>
-//       <li>Altura: ${filteredName["height"]}</li>
-//       <li>Peso: ${filteredName["weight"]}</li>
-//       <li>Egg: ${filteredName["egg"]}</li>
-//       <li>Spaw chance: ${filteredName["spawn_chance"]}</li>
-//       <li>AVG Spawns: ${filteredName["avg_spawns"]}</li>
-//       <li>Spawns time: ${filteredName["spawn_time"]}</li>
-//       <li>Fraquezas: ${filteredName["weaknesses"]}</li>
-//       </ul>
-//       </div>
-//       `
-//       searchResult.appendChild(newArticle); 
-//   } else {
-//     let newArticle = document.createElement("article");
-//     newArticle.innerHTML = `<p>:/ Pokemon não encontrado...</p>`
-//     searchResult.appendChild(newArticle); 
-//   }
-// }
+        newArticle.innerHTML = 
+        `
+        <div class="pokemon-card">
+          <img src="${pokemon["img"]}"></img>
+        </div>
+        <div class="pokemon-name">
+        <h3>${pokemon["name"]}</h3>
+        </div>
+        <div class="pokemon-info">
+          <ul>
+            <li>Tipo: ${pokemon["type"]}</li>
+            <li>Altura: ${pokemon["height"]}</li>
+            <li>Peso: ${pokemon["weight"]}</li>
+            <li>Egg: ${pokemon["egg"]}</li>
+            <li>Spaw chance: ${pokemon["spawn_chance"]}</li>
+            <li>AVG Spawns: ${pokemon["avg_spawns"]}</li>
+            <li>Spawns time: ${pokemon["spawn_time"]}</li>
+            <li>Fraquezas: ${pokemon["weaknesses"]}</li>
+          </ul>
+        </div>
+      `
+        searchResult.appendChild(newArticle);
+      
+    }
+  })
+}
 
-// srcBtn.addEventListener("click", findName);
+
+srcBtn.addEventListener("click", findName);
 returnHome.addEventListener("click", showAllCards);
 
 selectedLetter.addEventListener("change", () => {
