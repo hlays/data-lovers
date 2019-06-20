b708465a5a0af72f64610b64776d2e34ff0a9f26
layshidani <lays.hidani@gmail.com>
2019-06-20 17:21:38 -0300

    Fix bug donutChart

--- src/charts.js
+++ src/charts.js
@@ -1,7 +1,7 @@
-google.charts.load('current', { packages: ['corechart'] });
+google.charts.load("current", {packages:["corechart"]});
 google.charts.setOnLoadCallback(donutChart);
 
-google.charts.load('current', { packages: ['corechart', 'bar'] });
+google.charts.load("current", {packages:["corechart", "bar"]});
 google.charts.setOnLoadCallback(barChart);
 
 const pokemons = POKEMON.pokemon.map((pokemon) => pokemon);
@@ -11,10 +11,10 @@ const typeCount = (requiredType) => {
   pokemons.forEach((pokemon) => {
     pokemon.type.forEach((type) => {
       if (type === requiredType) {
-        totalTypeCount++;
+      totalTypeCount++;
       }
-    });
-  });
+    })
+  })
   return totalTypeCount;
 }
 
@@ -22,72 +22,72 @@ const weaknessesCount = (requiredWeaknesses) => {
   let totalWeaknesses = 0;
   pokemons.forEach((pokemon) => {
     pokemon.weaknesses.forEach((weaknesses) => {
-      if (weaknesses === requiredWeaknesses) {
+        if (weaknesses === requiredWeaknesses) {
         totalWeaknesses++;
-      }
-    });
-  });
+        }
+      })
+    })
   return totalWeaknesses;
 }
 
-const donutChart = () => {
-  const data = google.visualization.arrayToDataTable([
-    ['Task', 'Hours per Day'],
-    ['Normal', typeCount('Normal')],
-    ['Fogo', typeCount('Fire')],
-    ['Água', typeCount('Water')],
-    ['Elétrico', typeCount('Electric')],
-    ['Grama', typeCount('Grass')],
-    ['Gelo', typeCount('Ice')],
-    ['Lutador', typeCount('Fighting')],
-    ['Venenoso', typeCount('Poison')],
-    ['Terra', typeCount('Ground')],
-    ['Voador', typeCount('Flying')],
-    ['Psiquico', typeCount('Psychic')],
-    ['Inseto', typeCount('Bug')],
-    ['Pedra', typeCount('Rock')],
-    ['Fantasma', typeCount('Ghost')],
-    ['Dragão', typeCount('Dragon')]
+function donutChart() {
+  let data = google.visualization.arrayToDataTable([
+    ["Task", "Hours per Day"],
+    ["Normal", typeCount("Normal")],
+    ["Fogo", typeCount("Fire")],
+    ["Água", typeCount("Water")],
+    ["Elétrico", typeCount("Electric")],
+    ["Grama", typeCount("Grass")],
+    ["Gelo", typeCount("Ice")],
+    ["Lutador", typeCount("Fighting")],
+    ["Venenoso", typeCount("Poison")],
+    ["Terra", typeCount("Ground")],
+    ["Voador", typeCount("Flying")],
+    ["Psiquico", typeCount("Psychic")],
+    ["Inseto", typeCount("Bug")],
+    ["Pedra", typeCount("Rock")],
+    ["Fantasma", typeCount("Ghost")],
+    ["Dragão", typeCount("Dragon")]
   ]);
 
   const options = {
     pieHole: 0.4,
-    'width': 1000,
-    'height': 600,
-    backgroundColor: { fill: '#C6E9F0' },
+    "width": 1000,
+    "height": 600,
+    backgroundColor: { fill:"#C6E9F0" },
   };
 
-  const chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+  const chart = new google.visualization.PieChart(document.getElementById("donutchart"));
   chart.draw(data, options);
 }
 
-const barChart = () => {
+function barChart() {
   const data = google.visualization.arrayToDataTable([
-    ['Tipo', 'Quantidade', { role: 'style' }],
-    ['Normal', weaknessesCount('Normal'), '#407F7F'],
-    ['Fogo', weaknessesCount('Fire'), '#3AB795'],
-    ['Água', weaknessesCount('Water'), '#20A4F3'],
-    ['Elétrico', weaknessesCount('Electric'), '#E7FF51'],
-    ['Grama', weaknessesCount('Grass'), '#3AB795'],
-    ['Gelo', weaknessesCount('Ice'), '#16BAC5'],
-    ['Lutador', weaknessesCount('Fighting'), '#C97C5D'],
-    ['Venenoso', weaknessesCount('Poison'), '#5D2E8C'],
-    ['Terra', weaknessesCount('Ground'), '#47271A'],
-    ['Voador', weaknessesCount('Flying'), '#566FEA'],
-    ['Psiquico', weaknessesCount('Psychic'), '#F79071'],
-    ['Inseto', weaknessesCount('Bug'), '#002500'],
-    ['Pedra', weaknessesCount('Rock'), '#8C9A9E'],
-    ['Fantasma', weaknessesCount('Ghost'), '#E1BC29'],
-    ['Dragão', weaknessesCount('Dragon'), 'B7245C']
+    ["Tipo", "Quantidade", { role: "style" }],
+    ["Normal", weaknessesCount("Normal"), "#407F7F"],
+    ["Fogo", weaknessesCount("Fire"), "#3AB795"],
+    ["Água", weaknessesCount("Water"), "#20A4F3"],
+    ["Elétrico", weaknessesCount("Electric"), "#E7FF51"],
+    ["Grama", weaknessesCount("Grass"), "#3AB795"],
+    ["Gelo", weaknessesCount("Ice"), "#16BAC5"],
+    ["Lutador", weaknessesCount("Fighting"), "#C97C5D"],
+    ["Venenoso", weaknessesCount("Poison"), "#5D2E8C"],
+    ["Terra", weaknessesCount("Ground"), "#47271A"],
+    ["Voador", weaknessesCount("Flying"), "#566FEA"],
+    ["Psiquico", weaknessesCount("Psychic"), "#F79071"],
+    ["Inseto", weaknessesCount("Bug"), "#002500"],
+    ["Pedra", weaknessesCount("Rock"), "#8C9A9E"],
+    ["Fantasma", weaknessesCount("Ghost"), "#E1BC29"],
+    ["Dragão", weaknessesCount("Dragon"), "B7245C"]
   ]);
 
   const options = {
-    legend: 'none',
-    'width': 1000,
-    'height': 500,
-    backgroundColor: { fill: '#C6E9F0' },
+    legend: "none",
+    "width": 1000,
+    "height": 500,
+    backgroundColor: { fill:"#C6E9F0" },
   };
 
-  const chart = new google.visualization.BarChart(document.getElementById('barchart'));
+  const chart = new google.visualization.BarChart(document.getElementById("barchart"));
   chart.draw(data, options);
-}
+};
